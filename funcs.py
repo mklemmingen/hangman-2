8c2795de0448b6c47b05baf66bb91a39369fd069
Marty Lauterbach <LauterbachMK@protonmail.com>
2023-05-19 18:15:49 +0200

    formatted

--- funcs.py
+++ funcs.py
@@ -128,11 +128,11 @@ def guess_computer_letters_strategy(language_eng, gues_let_func: list):
 
     while True:
         in_function_guess = random.choices(text_assets.alphabet, weights=(8.4966, 2.0720, 4.5388, 3.3844, 11.1607,
-                                                                         1.8121, 2.4705, 3.0034, 7.5448, 0.1965,
-                                                                         1.1016, 5.4893, 3.0129, 6.6544, 7.1635,
-                                                                         3.1671, 0.1962, 7.5809, 5.7351, 6.9509,
-                                                                         3.6308, 1.0074, 1.2899, 0.2902, 1.7779,
-                                                                         0.2722), k=1)
+                                                                          1.8121, 2.4705, 3.0034, 7.5448, 0.1965,
+                                                                          1.1016, 5.4893, 3.0129, 6.6544, 7.1635,
+                                                                          3.1671, 0.1962, 7.5809, 5.7351, 6.9509,
+                                                                          3.6308, 1.0074, 1.2899, 0.2902, 1.7779,
+                                                                          0.2722), k=1)
         if in_function_guess in gues_let_func:
             continue
         if in_function_guess not in gues_let_func:
